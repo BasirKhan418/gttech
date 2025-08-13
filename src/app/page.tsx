63d6f97a9c7eb2432d61d39e8351759af7326da0
Aniket Subudhi <aniketsubudhi00@gmail.com>
2025-08-14 00:55:32 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import Navbar from '@/components/Navbar'
 import HeroSection from '@/components/HeroSection'
 import ServicesPreview from '@/components/ServicesPreview'
 import TechnologiesSection from '@/components/TechnologiesSection'
-import Footer from '@/components/Footer'
 import DynamicBannerSection from '@/components/banner'
 
 const HomePage = () => {
   return (
     <main className="min-h-screen bg-black">
-      <Navbar />
       <HeroSection />
       <TechnologiesSection />
       <DynamicBannerSection/>
       <ServicesPreview />
-      <Footer />
     </main>
   )
 }
