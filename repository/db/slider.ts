d8351906ade7fcada38f98790aaac3fbb00bcaea
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-08-14 01:53:20 +0530

    feat:slider crud added

---
+++ repository/db/slider.ts
@@ -0,0 +1,49 @@
+import Slider from "../../models/Slider";
+import ConnectDb from "../../middlewares/connectdb";
+
+
+const getSlider = async () => {
+    try{
+        await ConnectDb();
+        let data = await Slider.find();
+        return { success: true, data };
+    }
+    catch(err) {
+        console.error(err);
+        return { success: false, message: "Error fetching slider. Please try again later." };
+    }
+}
+
+const addSlider = async(data:any)=>{
+    try{
+        let newSlider = new Slider(data);
+        await newSlider.save();
+        return { success: true, message: "Slider added successfully." };
+    }
+    catch(err){
+     return { success: false, message: "Error adding slider. Please try again later." };
+    }
+}
+
+const updateSlider = async (id: string, data: any) => {
+    try{
+        let update = await Slider.findByIdAndUpdate(id, data, { new: true });
+        return { success: true, data: update };
+    }
+    catch(err){
+        return { success: false, message: "Error updating slider. Please try again later." };
+    }
+}
+
+
+const deleteSlider = async (id: string) => {
+    try{
+        await Slider.findByIdAndDelete(id);
+        return { success: true, message: "Slider deleted successfully." };
+    }
+    catch(err){
+        return { success: false, message: "Error deleting slider. Please try again later." };
+    }
+}
+
+export {getSlider, addSlider, updateSlider,deleteSlider}
---
+++ src/app/api/slider/route.ts
@@ -0,0 +1,73 @@
+import { NextResponse,NextRequest } from "next/server";
+import { getSlider,addSlider,updateSlider,deleteSlider } from "../../../../repository/db/slider";
+import { cookies } from "next/headers";
+import VerifyJwt from "../../../../utils/VerifyJwt";
+
+export const GET = async (req: NextRequest, res: NextResponse) => {
+    try{
+        let response  = await getSlider();
+        return NextResponse.json(response);
+    }
+    catch(err){
+        return NextResponse.json({ success: false, message: "Error fetching slider. Please try again later." });
+    }
+}
+
+export const POST = async (req: NextRequest, res: NextResponse) => {
+    try {
+        let getobj = await cookies();
+        let token = getobj.get("token");
+        let resultjst = await VerifyJwt(token?.value ?? "");
+        //@ts-ignore
+        const userdata = await finduser(resultjst?.data?.email);
+
+        const data  =await req.json();
+        const content = await addSlider({...data,lastEditedAuthor: userdata.data._id,author: userdata.data._id});
+        console.log("Content created:", content);
+        if (content.success) {
+            return NextResponse.json({ success: true }, { status: 201 });
+        }
+        return NextResponse.json({ success: false, message: "Error creating content. Please try again later." }, { status: 500 });
+    } catch (err: any) {
+        console.log("error is ", err.message);
+        return NextResponse.json({ success: false, message: "Error processing request. Please try again later." }, { status: 500 });
+    }
+}
+
+
+export const PUT = async (req: NextRequest, res: NextResponse) => {
+    try{
+        const data = await req.json();
+         let getobj = await cookies();
+        let token = getobj.get("token");
+        let resultjst = await VerifyJwt(token?.value ?? "");
+        //@ts-ignore
+        const userdata = await finduser(resultjst?.data?.email);
+        const content = await updateSlider(data.id,{...data,lastEditedAuthor: userdata.data._id});
+        if (content.success) {
+            return NextResponse.json({ success: true, data: content.data }, { status: 200 });
+        }
+        return NextResponse.json({ success: false, message: "Error updating content. Please try again later." }, { status: 500 });
+    }
+    catch(err: any) {
+        return NextResponse.json({ success: false, message: "Error processing request. Please try again later." }, { status: 500 });
+    }
+}
+
+
+export const DELETE = async (req: NextRequest, res: NextResponse) => {
+    try{
+        const data = await req.json();
+        //@ts-ignore
+        const content = await deleteSlider(data.id);
+        if (content.success) {
+            return NextResponse.json({ success: true, message:"Content deleted successfully." }, { status: 200 });
+        }
+        return NextResponse.json({ success: false, message: "Error deleting content. Please try again later." }, { status: 500 });
+    }
+    catch(err: any) {
+        return NextResponse.json({ success: false, message: "Error processing request. Please try again later." }, { status: 500 });
+    }
+}
+
+
