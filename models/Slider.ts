c68df2b0182d16cc5fa760df24bd2dafaa8eaf5e
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-08-13 15:49:27 +0530

    init:slider schema added

---
+++ models/Slider.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const SliderSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: false },
+    lastEditedAuthor: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', required: true },
+}, { timestamps: true })
+export default mongoose.models.Slider || mongoose.model('Slider', SliderSchema);
